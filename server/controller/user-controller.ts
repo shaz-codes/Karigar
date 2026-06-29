37a43cbe05ad3a93e1298221ec438cb5f547c0ef
shaz-codes <sb986257@gmail.com>
2026-06-29 23:59:31 +0530

    middleware setup

--- server/controller/user-controller.ts
+++ server/controller/user-controller.ts
@@ -1,3 +1,5 @@
 import Express from "express";
 
 const app = Express();
+
+app.post("/signup");
--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,4 +1,10 @@
 import Express from "express";
+import JWT from "jsonwebtoken";
 const app = Express();
 
-app.get();
+app.use((req, res, next) => {
+	const token = req.cookies.jwt;
+	const jwt = JWT.verify(token, "meow");
+	console.log(jwt);
+	next();
+});
