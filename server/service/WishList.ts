7a853e0d63a4e5d7c18fe6f3405fa9de8ebfa02b
shaz-codes <sb986257@gmail.com>
2026-07-25 00:41:37 +0530

    wip2: wishlist backend

--- server/service/WishList.ts
+++ server/service/WishList.ts
@@ -1,19 +1,42 @@
 import User from "./../models/User";
 import Wishlist from "../models/Wishlist";
 import Product from "../models/Product";
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 
-export async function(req: Request, res: Response){
-    console.log("wishlist");
+export async function GetWishlist(req: Request, res: Response) {
+	console.log("wishlist");
 
-    try {
-        const { email, sku } = req.body;
-        const user = await User.findOne(email);
-        const product=await Product.findOne(sku);
-    }
+	try {
+		const email = req.user.email;
+		const A = await User.findOne(email);
+		await Wishlist.find({ user: A?._id }).populate("Product");
+	} catch (err: any) {
+		return res.status(500).send({ error: err.message });
+	}
+}
 
-    catch(err:any){
-        return res.status(500).send({ error: err.message });
-    }
-    
+export async function AddWishlist(req: Request, res: Response) {
+	try {
+		const email = req.user.email;
+		const A = await User.findOne(email);
+		const { sku } = req.body;
+		const B = await Product.findOne({ sku });
+		const pro = await Wishlist.create({ user: A?._id, product: B?._id });
+		return res.status(201).send();
+	} catch (err: any) {
+		return res.status(500).send({ error: err.message });
+	}
+}
+
+export async function RemoveProduct(req: Request, res: Response) {
+	try {
+		const email = req.user.email;
+		const A = await User.findOne(email);
+		const { sku } = req.body;
+		const B = await Product.findOne({ sku });
+		const pro = await Wishlist.deleteOne({ user: A?._id, product: B?._id });
+		return res.status(200).send();
+	} catch (err: any) {
+		return res.status(500).send({ error: err.message });
+	}
 }
